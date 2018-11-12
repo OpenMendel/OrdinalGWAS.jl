af8a9e918903bbfbc5f8c58798716b1c1465d821
Hua Zhou <huazhou@ucla.edu>
2018-11-12 12:46:19 -0800

    add build.jl script for dependency on unregistered SnpArrays and PolrModels pacakges

---
+++ deps/build.jl
@@ -0,0 +1,4 @@
+using Pkg
+
+Pkg.add(PackageSpec(url="https://github.com/OpenMendel/SnpArrays.jl.git", rev="juliav0.7"))
+Pkg.add(PackageSpec(url="https://github.com/OpenMendel/PolrModels.jl.git", rev="master"))
