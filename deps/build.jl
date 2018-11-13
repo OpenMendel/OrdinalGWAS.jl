6211afcc7b5b78a74b2e874e1245f28c731fee9d
Hua Zhou <huazhou@ucla.edu>
2018-11-12 17:04:49 -0800

    build.jl does not resolve Travis issue with unregistered pkgs becoz it's run after resolve

--- deps/build.jl
+++
@@ -1,4 +0,0 @@
-using Pkg
-
-Pkg.add(PackageSpec(url="https://github.com/OpenMendel/SnpArrays.jl.git", rev="juliav0.7"))
-Pkg.add(PackageSpec(url="https://github.com/OpenMendel/PolrModels.jl.git", rev="master"))
