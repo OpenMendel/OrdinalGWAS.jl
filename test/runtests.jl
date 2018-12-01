cc84c61c8d0e2abcf2ea489d23c4de52f0bdf4e2
Hua Zhou <huazhou@ucla.edu>
2018-12-01 10:45:12 -0800

    re-run tutorial/documentation and adjust testing responding to https://github.com/OpenMendel/PolrModels.jl#3

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,7 +9,7 @@ const plkfile = datadir * "/hapmap3"
     @test isfile("polrgwas.nullmodel.txt")
     @test isfile("polrgwas.scoretest.txt")
     scorepvals = CSV.read("polrgwas.scoretest.txt")[5][1:5]
-    @test all(scorepvals .≈ [1.0, 3.00123075e-3, 2.51172149e-5, 1.137311225e-5, 8.31735837e-3])
+    @test isapprox(scorepvals, [1.0, 4.56531284e-3, 3.10828383e-5, 1.21686724e-5, 8.20686005e-3], rtol=1e-4)
     rm("polrgwas.nullmodel.txt")
     rm("polrgwas.scoretest.txt")
 end
@@ -19,7 +19,7 @@ end
     @test isfile("polrgwas.nullmodel.txt")
     @test isfile("polrgwas.lrttest.txt")
     lrtpvals = CSV.read("polrgwas.lrttest.txt")[6][1:5]
-    @test all(lrtpvals .≈ [1.0, 1.91858366e-3, 1.80505056e-5, 5.87338471e-6, 8.08102258e-3])
+    @test isapprox(lrtpvals, [1.0, 1.91858366e-3, 1.80505056e-5, 5.87338471e-6, 8.08102258e-3], rtol=1e-4)
     rm("polrgwas.nullmodel.txt")
     rm("polrgwas.lrttest.txt")
 end
@@ -30,7 +30,7 @@ end
     @test isfile("polrgwas.nullmodel.txt")
     @test isfile("polrgwas.scoretest.txt")
     scorepvals = CSV.read("polrgwas.scoretest.txt")[5][1:5]
-    @test all(scorepvals .≈ [1.0, 1.24849027e-1, 4.08779119e-4, 5.19281523e-3, 7.04007765e-4])
+    @test isapprox(scorepvals, [1.0, 0.14295, 0.000471942, 0.00555348, 0.000652844], rtol=1e-4)
     rm("polrgwas.nullmodel.txt")
     rm("polrgwas.scoretest.txt")
     # recessive model 
@@ -38,7 +38,7 @@ end
     @test isfile("polrgwas.nullmodel.txt")
     @test isfile("polrgwas.scoretest.txt")
     scorepvals = CSV.read("polrgwas.scoretest.txt")[5][1:5]
-    @test all(scorepvals .≈ [1.0, 4.56151864e-3, 2.77072395e-4, 4.25357392e-5, 1.66390827e-1])
+    @test isapprox(scorepvals, [1.0, 0.00673612, 0.000279908, 4.15322e-5, 0.167642], rtol=1e-4)
     rm("polrgwas.nullmodel.txt")
     rm("polrgwas.scoretest.txt")
 end
@@ -48,7 +48,7 @@ end
     @test isfile("opm.nullmodel.txt")
     @test isfile("opm.scoretest.txt")
     scorepvals = CSV.read("opm.scoretest.txt")[5][1:5]
-    @test all(scorepvals .≈ [1.0, 6.45042592e-3, 1.57850425e-5, 4.97907512e-6, 4.56657402e-3])
+    @test isapprox(scorepvals, [1.0, 1.00769167e-2, 2.62725649e-5, 1.08974849e-5, 5.10288399e-3], rtol=1e-4)
     rm("opm.nullmodel.txt")
     rm("opm.scoretest.txt")
 end
@@ -59,7 +59,7 @@ end
     @test isfile("first5snps.scoretest.txt")
     @test countlines("first5snps.scoretest.txt") == 6
     scorepvals = CSV.read("first5snps.scoretest.txt")[5]
-    @test all(scorepvals .≈ [1.0, 3.00123075e-3, 2.51172149e-5, 1.137311225e-5, 8.31735837e-3])
+    @test isapprox(scorepvals, [1.0, 4.56531284e-3, 3.10828383e-5, 1.21686724e-5, 8.20686005e-3], rtol=1e-4)
     rm("first5snps.nullmodel.txt")
     rm("first5snps.scoretest.txt")
 end
@@ -70,7 +70,7 @@ end
     @test isfile("GxE.nullmodel.txt")
     @test isfile("GxE.scoretest.txt")
     scorepvals = CSV.read("GxE.scoretest.txt")[5][1:5]
-    @test all(scorepvals .≈ [1.0, 1.17883709e-2, 1.38956191e-4, 4.44624250e-5, 2.94881322e-2])
+    @test isapprox(scorepvals, [1.0, 1.74460104e-2, 1.66707324e-4, 4.76376246e-5, 2.91384712e-2], rtol=1e-4)
     rm("GxE.nullmodel.txt")
     rm("GxE.scoretest.txt")
     # LRT, only first 5 SNPs
@@ -79,7 +79,8 @@ end
     @test isfile("GxE.nullmodel.txt")
     @test isfile("GxE.lrttest.txt")
     lrtpvals = CSV.read("GxE.lrttest.txt")[end]
-    @test all(lrtpvals .≈ [1.0, 7.22410973e-3, 1.01730983e-4, 1.88174211e-5, 2.88295705e-2])
+    @show lrtpvals
+    @test isapprox(lrtpvals, [1.0, 7.22410973e-3, 1.01730983e-4, 1.88174211e-5, 2.88295705e-2], rtol=1e-4)
     rm("GxE.nullmodel.txt")
     rm("GxE.lrttest.txt")
 end
